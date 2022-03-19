492e048af29081246988d9ce400adcb3759077c8
James Srigiri <jamessrigiri@gmail.com>
2022-03-19 06:57:44 +0530

    Deploying v1.0.0 of the logistics application

---
+++ index.js
@@ -0,0 +1,342 @@
+require("dotenv").config()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const path = require("path")
+const PORT = process.env.PORT || 8000
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+app.set("view engine", "pug")
+app.use(express.static("public"))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Authirized Users
+const authorizedUsers = [
+  "9573784242_1234",
+  "9866074207_1213"
+]
+
+// Notion data
+const { Client } = require("@notionhq/client")
+
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+app.get("/", async (req, res) => {
+  res.json(`Welcome to the QR CODE APP.`)
+})
+
+app.get("/login/:username", loginUser, async(req,res) => {
+  res.json(req.response)
+})
+
+app.get("/:qrid", async (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public/views") })
+})
+
+app.get("/qrscan/:qrid", authUser, filterObjectGenerator, async (req, res) => {
+  const response = await getItem(req.body.filterObject)
+  res.json(response)
+})
+
+app.post("/qrscan/:action", authUser, itemObjectGenerator, filterObjectGenerator, async (req, res) => {
+    if (req.params.action == "addNew") {
+      const response = await addNewItem(req.body.itemObject)
+      res.json(response)
+    }
+    else if (req.params.action == "editOld") {
+      const filteredResult = await getItem(req.body.filterObject)
+      if (filteredResult.results.length == 0) {
+        const response = {}
+        res.json(response)
+      }
+      else {
+        const pageId = filteredResult.results[0]["id"]
+        const response = await editOldItem(req.body.itemObject, pageId)
+        res.json(response)
+      }
+    }
+
+    // res.json(`The main page where you get the qrid information if it exists and perhaps provide options to edit if needed`)
+  }
+)
+
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
+
+function loginUser(req, res, next){
+  if(authorizedUsers.includes(req.params.username)){
+    res.cookie('username',req.params.username,{
+      maxAge:3600000
+    })
+    req.response = {"response": "AUTHORIZED"}
+  }
+  else{
+    res.clearCookie('username')
+    req.response = {"response": "ACCESS DENIED"}
+  }
+  next()
+}
+
+function authUser(req, res, next){
+  const frontEndUser = req.cookies.username
+  if(!authorizedUsers.includes(frontEndUser)){
+    res.clearCookie('username')
+    return res.sendFile("index.html", { root: path.join(__dirname, "public/views") })
+  }
+  next()
+}
+
+function itemObjectGenerator(req, res, next) {
+  const requestBody = req.body
+  var templateObject = {
+    "Destination Details": {
+      id: "%3DvTo",
+      type: "rich_text",
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["DestinationDetails"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["DestinationDetails"],
+          href: null,
+        },
+      ],
+    },
+    "Fragile": {
+      id: "%3FF%5CR",
+      type: "checkbox",
+      checkbox: requestBody["Fragile"],
+    },
+    "Truck Orientation": {
+      id: "ELMv",
+      type: "select",
+      select: {
+        name: requestBody["TruckOrientation"],
+      },
+    },
+    "Category": {
+      id: "EZ%60U",
+      type: "multi_select",
+      multi_select: multiSelectObjectGenerator(requestBody["Category"]),
+    },
+    "Destination": {
+      id: "%5CXtx",
+      type: "select",
+      select: {
+        name: requestBody["Destination"],
+      },
+    },
+    "Contents": {
+      id: "%5EPe%60",
+      type: "rich_text",
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["Contents"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["Contents"],
+          href: null,
+        },
+      ],
+    },
+    "Floor Number": {
+      id: "wwyY",
+      type: "rich_text",
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["FloorNumber"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["FloorNumber"],
+          href: null,
+        },
+      ],
+    },
+    "Status": {
+      id: "%7D%3Dev",
+      type: "select",
+      select: {
+        name: requestBody["Status"],
+      },
+    },
+    "Mode": {
+      id: "%3B%3AsT",
+      type: "select",
+      select: {
+        name: requestBody["Mode"],
+      },
+    },
+    "QRCID": {
+      id: "title",
+      type: "title",
+      title: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["QRCID"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["QRCID"],
+          href: null,
+        },
+      ],
+    },
+  }
+
+  if(requestBody["EntryCode"] !== undefined){
+    templateObject["Entry Code"] = {
+      id: "ZwKV",
+      type: "rich_text",
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["EntryCode"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["EntryCode"],
+          href: null,
+        },
+      ],
+    }
+  }
+  if(requestBody["EditCode"] !== undefined){
+    templateObject["Edit Code"] = {
+      id: "fZgh",
+      type: "rich_text",
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: requestBody["EditCode"],
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: requestBody["EditCode"],
+          href: null,
+        },
+      ],
+    }
+  }
+
+  req.body.itemObject = templateObject
+  req.params.qrid = requestBody["QRCID"]
+  next()
+}
+
+function filterObjectGenerator(req, res, next) {
+  const requestID = req.params.qrid
+  const templateObject = {
+    property: "QRCID",
+    title: {
+      equals: requestID,
+    },
+  }
+  req.body.filterObject = templateObject
+  next()
+}
+
+function multiSelectObjectGenerator(multiSelectArray = []) {
+  const multiSelectObject = []
+  multiSelectArray.forEach((option) => {
+    multiSelectObject.push({ name: option })
+  })
+  return multiSelectObject
+}
+
+async function addNewItem(propertiesObject) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: propertiesObject,
+    })
+    console.log(response)
+    return response
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+async function getItem(filterObject) {
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: filterObject,
+    })
+    console.log(response)
+    return response
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+async function editOldItem(propertiesObject, pageId) {
+  try {
+    const response = await notion.pages.update({
+      page_id: pageId,
+      database_id: databaseId,
+      properties: propertiesObject,
+    })
+    console.log(response)
+    return response
+  } catch (error) {
+    console.error(error.body)
+  }
+}
---
+++ public/assets/js/main_cs.js
@@ -0,0 +1,199 @@
+// Globals
+USERNAME = ""
+PRIMARY_URL ="http://localhost:8000/"
+// Adding Event Listeners
+document.getElementById("main-icon").addEventListener("click",loginUser)
+document.getElementById("form-submit-button").addEventListener("click",submitForm)
+
+cookieCheck()
+
+async function cookieCheck(message="Please Login"){
+    if (document.cookie.includes("username=")) {
+        const cookiesArray = document.cookie.split(";")
+        cookiesArray.forEach(cookie => {
+            if (cookie.includes("username=")){
+                USERNAME = cookie.replace("username=","").trim()
+            }
+        })
+        loadingAnimationToggle()
+        await checkData()
+        loadingAnimationToggle()
+      document.getElementById("user-code").value = USERNAME
+      document.getElementById("main-details-form").style.maxHeight = "99999px"
+      
+    }
+    else{
+        document.getElementById("main-details-form").style.maxHeight = "0px"
+        alert (message)
+    }
+}
+
+// All Functions
+// User Login
+async function loginUser(){
+    const username = document.getElementById("user-code").value.toString()
+    const url = PRIMARY_URL + "login/" + username
+    loadingAnimationToggle()
+    const loginResponse = await webAppCalls(url)
+    loadingAnimationToggle()
+    await cookieCheck(loginResponse.response)  
+}
+
+// Data Calls
+async function checkData(){
+    const qrid = window.location.pathname.replace("/","").trim()
+    const url = PRIMARY_URL + "qrscan/" + qrid
+    const response = await webAppCalls(url)
+    console.log(response.results)
+    if(response.results.length !== 0){
+        await editOldItemSetup(response.results[0])
+    }
+}
+
+async function editOldItemSetup(previousDetails){
+    document.getElementById("information-section-header").innerHTML = "EDIT PREVIOUS ENTRY"
+    const previousEntryData = previousDetails.properties
+    console.log(previousEntryData)
+    const forAlert_editCode = deconstruct(previousEntryData,"Edit Code").length !== 0 ? deconstruct(previousEntryData,"Edit Code")[0]['plain_text'] : "NO EDITS"
+    const metaDetails = `Entry Code: ${deconstruct(previousEntryData,"Entry Code")[0]['plain_text']}: ${utcToIst(deconstruct(previousEntryData,"Added Date"))}\nEdit Code: ${forAlert_editCode}: ${utcToIst(deconstruct(previousEntryData,"Last Edited Date"))}`
+    window.alert(metaDetails)
+    // Modifying values in the form
+    document.getElementsByName("floor-number")[0].value = deconstruct(previousEntryData,"Floor Number")[0]['plain_text']
+    document.getElementsByName("mode")[0].value = deconstruct(previousEntryData,"Mode")['name']
+    deconstruct(previousEntryData,"Category").forEach(category => {
+        document.getElementsByName(category["name"])[0].checked = true
+    })
+    document.getElementsByName("contents")[0].value = deconstruct(previousEntryData,"Contents")[0]['plain_text']
+    document.getElementsByName("fragile")[0].checked = deconstruct(previousEntryData, "Fragile")
+    document.getElementsByName("truck-orientation")[0].value = deconstruct(previousEntryData,"Truck Orientation")['name']
+    document.getElementsByName("status")[0].value = deconstruct(previousEntryData,"Status")['name']
+    document.getElementsByName("destination")[0].value = deconstruct(previousEntryData,"Destination")['name']
+    document.getElementsByName("destination-details")[0].value = deconstruct(previousEntryData,"Destination Details")[0]['plain_text']
+}
+
+// Submit Form
+async function submitForm(){
+    loadingAnimationToggle()
+    const submitType = await checkDataPreSub()
+    const propertiesData = propertiesObjectGenerator(submitType)
+    const action = submitType==0 ? "addNew" : "editOld"
+    const url = PRIMARY_URL + "qrscan/" + action
+    const response = await webAppCalls(url,"POST", propertiesData)
+    loadingAnimationToggle()
+    window.alert("Submission Successful!")    
+    location.reload()  
+}
+
+// Submission pre check
+async function checkDataPreSub(){
+    const qrid = window.location.pathname.replace("/","").trim()
+    const url = PRIMARY_URL + "qrscan/" + qrid
+    const response = await webAppCalls(url)
+    return response.results.length 
+}
+
+
+// Web App Calls
+async function webAppCalls(url,method="GET",query={}){
+  if(method == 'GET'){
+      const respval = await fetch(url)
+      .then(res => res.json())
+      .then(resp => {
+          return resp
+      })
+      .catch(error => console.log("Error: " + error))
+      return respval
+  }
+  if(method == "POST"){
+      const respval = await fetch(url,{
+          method: 'POST',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify(query)
+      })
+      .then(res => res.json())
+      .then(resp => {
+          return resp
+      })
+      .catch(error => console.log("Error: " + error))
+      return respval
+  }
+}
+
+// Properties Object Generator
+function propertiesObjectGenerator(submitType){
+    const submitCode = usernameCookie()
+    const val_QRCID = window.location.pathname.replace("/","").trim()
+    const val_FloorNumber = document.getElementsByName("floor-number")[0].value
+    const val_Mode = document.getElementsByName("mode")[0].value
+    var val_Category = []
+    var temp_category_array = document.getElementsByClassName("category-cb")
+    Array.prototype.forEach.call(temp_category_array, function(category) {
+        if(category.checked){
+            val_Category.push(category.name)
+        }
+    })
+    const val_Contents = document.getElementsByName("contents")[0].value
+    const val_Fragile = document.getElementsByName("fragile")[0].checked
+    const val_TruckOrientation = document.getElementsByName("truck-orientation")[0].value
+    const val_Status = document.getElementsByName("status")[0].value
+    const val_Destination = document.getElementsByName("destination")[0].value
+    const val_DestinationDetails = document.getElementsByName("destination-details")[0].value
+    const propertiesObject = {
+        "DestinationDetails": val_DestinationDetails,
+        "Fragile": val_Fragile,
+        "TruckOrientation": val_TruckOrientation,
+        "Category": val_Category,
+        "Destination":val_Destination,
+        "Contents": val_Contents,
+        "FloorNumber": val_FloorNumber,
+        "Status": val_Status,
+        "Mode":val_Mode,
+        "QRCID": val_QRCID
+    }
+    if(submitType == 0){
+        propertiesObject["EntryCode"] = submitCode
+    }
+    else{
+        propertiesObject["EditCode"] = submitCode
+    }
+    return propertiesObject    
+}
+
+// Loading Animation Toggle
+function loadingAnimationToggle(){
+    if (document.getElementById("main-icon").className.includes(" loading")){
+        document.getElementById("main-icon").className = document.getElementById("main-icon").className.replace(" loading","")
+    }
+    else{
+        document.getElementById("main-icon").className = document.getElementById("main-icon").className + " loading"
+    }
+}
+
+// Response Deconstructor
+function deconstruct(properties,property){
+    return properties[property][properties[property]['type']]
+}
+
+// Convert UTC to readable date and time
+function utcToIst(dateValue){
+    const date = new Date(dateValue)
+    return `${pad(date.getDate(),2)}-${pad(date.getMonth()+1,2)}-${date.getYear()+1900} ${pad(date.getHours(),2)}:${pad(date.getMinutes(),2)}`  
+}
+
+// Zeropad for max 10 digits
+function pad(num,size){
+    const s = "000000000"+num
+    return s.substring(s.length-size)  
+}
+
+// Get username cookie
+function usernameCookie(){
+    var username = ""
+    const cookiesArray = document.cookie.split(";")
+    cookiesArray.forEach(cookie => {
+        if (cookie.includes("username=")){
+            username = cookie.replace("username=","").trim()
+        }
+    })
+    return username
+}
